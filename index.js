adf583b62aa975c27f1edf1a6529333336b8146f
AmanSharmaDuredev <aman.sharma@dure.dev>
2024-12-22 02:41:05 +0530

    Tutorial 10 - Module Exports

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-require("./add");
+const addFn = require("./add");
 
-console.log("Hello world from index.js");
+console.log("Hello world from index.js" );
+
+const sum = addFn(3, 4);
+const sum2 = addFn(5, 6);
+console.log(sum);
+console.log(sum2);
