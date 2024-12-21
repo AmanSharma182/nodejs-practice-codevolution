da2ec8666328973816fa718a6942c71fef65fd3a
AmanSharmaDuredev <aman.sharma@dure.dev>
2024-12-22 02:48:42 +0530

    Tutorial 11 - Module Scope

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-const addFn = require("./add");
-
-console.log("Hello world from index.js" );
-
-const sum = addFn(3, 4);
-const sum2 = addFn(5, 6);
-console.log(sum);
-console.log(sum2);
+require("./batman");
+require("./superman");
