7a91977838aaea018bc6be4d5ba7e90a1885e179
AmanSharmaDuredev <aman.sharma@dure.dev>
2024-12-31 02:42:47 +0530

    Tutorial 32 - JSON Response

--- http.js
+++ http.js
@@ -1,10 +1,14 @@
 const http = require("node:http")
 
 const server = http.createServer((req, res) => {
+  const superHero = {
+    firstName: "Bruce",
+    lastName: "Wayne"
+  }
   res.writeHead(200, {
-    "Content-Type": "text/plain"
+    "Content-Type": "application/json"
   })
-  res.end("My first servers");
+  res.end(JSON.stringify(superHero));
 })
 
 server.listen(3000, () => {
